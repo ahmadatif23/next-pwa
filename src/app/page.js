c71218628693046ec8a1d34f5f06d8cba05708ae
ahmadatif23 <ahmadatif23@gmail.com>
2023-08-24 15:59:32 +0800

    clear start

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center text-center h-screen w-screen bg-white">
+    <main className="flex flex-col items-center justify-center text-center h-screen w-screen bg-slate-50">
       <h1 className='text-4xl'>Hello</h1>
     </main>
   )
